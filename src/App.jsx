5250ea957c4d6ae05bb9148530ddd8d987387b8e
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-11 21:31:45 +0300

    8

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      hello world
-      <p className="first-paragraph">This is the first paragraph</p>
-      <p className="second-paragraph">This is the second paragraph</p>
-      <p className="third-paragraph">This is the third paragraph</p>
-    </div>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+      <li>Item 4</li>
+      <li>Item 5</li>
+      <li>Item 6</li>
+      <li>Item 7</li>
+      <li>Item 8</li>
+      <li>Item 9</li>
+      <li>Item 10</li>
+    </ul>
   );
 }
 
