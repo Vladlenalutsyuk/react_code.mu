5ed753723e2fc1622fb7f7306feac9ee0f13640e
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-11 21:30:31 +0300

    7

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function App() {
-	return <div>
-		text
-	</div>;
+  return (
+    <div>
+      hello world
+      <p className="first-paragraph">This is the first paragraph</p>
+      <p className="second-paragraph">This is the second paragraph</p>
+      <p className="third-paragraph">This is the third paragraph</p>
+    </div>
+  );
 }
 
 export default App;
