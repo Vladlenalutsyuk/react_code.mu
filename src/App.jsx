d134326164f68c33c73f24005a2c978b19602b4d
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-12 05:32:04 +0300

    24_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 function App() {
-  return (
-    <div>
-      <table>
-        <tbody>
-          <tr>
-            <td>Row 1, Cell 1</td>
-            <td>Row 1, Cell 2</td>
-            <td>Row 1, Cell 3</td>
-          </tr>
-          <tr>
-            <td>Row 2, Cell 1</td>
-            <td>Row 2, Cell 2</td>
-            <td>Row 2, Cell 3</td>
-          </tr>
-          <tr>
-            <td>Row 3, Cell 1</td>
-            <td>Row 3, Cell 2</td>
-            <td>Row 3, Cell 3</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+	const num1 = 3;
+	const num2 = 2;
+	
+	return <div>
+		result: {num1 ** num2}
+	</div>;
 }
+//9
 
 export default App;
