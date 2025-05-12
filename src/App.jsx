df35b3c1f73998f71d9ac2a04b19690884526efe
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-12 05:30:11 +0300

    23_1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <input />
-      <br />
-      <input />
-      <br />
-      <input />
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+        <li>Item 6</li>
+        <li>Item 7</li>
+        <li>Item 8</li>
+        <li>Item 9</li>
+        <li>Item 10</li>
+      </ul>
     </div>
   );
 }
