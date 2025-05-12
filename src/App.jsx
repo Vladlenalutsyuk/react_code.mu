492ad2c6fc64e3662b64a6941f439ee0a2dd7e84
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-12 05:11:58 +0300

    20

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 function App() {
-  const li1 = <li>text1</li>;
-  const li2 = <li>text2</li>;
-  const li3 = <li>text3</li>;
-
-  return (
-    <ul>
-      {li1}
-      {li2}
-      {li3}
-    </ul>
+  const items = (
+    <>
+      <li>text1</li>
+      <li>text2</li>
+      <li>text3</li>
+    </>
   );
+  
+  return <ul>{items}</ul>;
 }
 
 export default App;
