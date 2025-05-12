9938e280f2c511388578aee29d55994c71273ae9
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-12 04:59:33 +0300

    14

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 function App() {
-	const str1 = 'text1';
-	const str2 = 'text1';
+	const name = 'user';
+	const age  = '30';
 	
 	return <div>
-		<p>{str1}</p>
-		<p>{str2}</p>
+		<p>name: {name}</p>
+		<p>age:  {age}</p>
 	</div>;
 }
+
+export default App;
