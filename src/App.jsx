4917528e3e845a2e3e9d1caf2062f3fbb7099e5c
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-15 20:58:29 +0300

    41

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 function App() {
-  const arr = ['a', 'b', 'c', 'd', 'e'];
-  
+  const users = [
+    {name: 'user1', surn: 'surn1', age: 30},
+    {name: 'user2', surn: 'surn2', age: 31},
+    {name: 'user3', surn: 'surn3', age: 32},
+  ];
+
   return (
     <ul>
-      {arr.map((item, index) => (
-        <li key={index}>{item}</li>
+      {users.map((user, index) => (
+        <li key={index}>
+          {user.name} {user.surn}, возраст: {user.age}
+        </li>
       ))}
     </ul>
   );
