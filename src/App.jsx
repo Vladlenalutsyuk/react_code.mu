2ca91579a157545e2c9ce9b98c792757a8d86a30
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-15 20:59:10 +0300

    42

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 function App() {
   const users = [
-    {name: 'user1', surn: 'surn1', age: 30},
-    {name: 'user2', surn: 'surn2', age: 31},
-    {name: 'user3', surn: 'surn3', age: 32},
+    {id: 1, name: 'user1', surn: 'surn1', age: 30},
+    {id: 2, name: 'user2', surn: 'surn2', age: 31},
+    {id: 3, name: 'user3', surn: 'surn3', age: 32},
   ];
 
   return (
     <ul>
-      {users.map((user, index) => (
-        <li key={index}>
+      {users.map(user => (
+        <li key={user.id}>
           {user.name} {user.surn}, возраст: {user.age}
         </li>
       ))}
