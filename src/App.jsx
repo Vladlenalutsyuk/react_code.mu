f57b1eabf78d4e9e005adcdf549e513242d51881
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-15 20:53:06 +0300

    38

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 function App() {
-  const arr = [
-    <li key={1}>1</li>,
-    <li key={2}>2</li>,
-    <li key={3}>3</li>,
-    <li key={4}>4</li>,
-    <li key={5}>5</li>,
-  ];
+  const items = []; // Создаем пустой массив для хранения <li> элементов
+  
+  // Заполняем массив в цикле
+  for (let i = 1; i <= 5; i++) {
+    items.push(<li key={i}>{i}</li>); // Добавляем <li> с уникальным key
+  }
   
   return (
     <ul>
-      {arr}
+      {items}
     </ul>
   );
 }
