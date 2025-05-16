daa114d5f468ce52c270956bfc91e01220eb8ccf
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-16 13:00:35 +0300

    68_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
 import React from 'react';
 
 function App() {
-  const [ageGroup, setAgeGroup] = useState('');
+  const [selectedValue, setSelectedValue] = useState('1');
   
   const handleChange = (event) => {
-    setAgeGroup(event.target.value);
+    setSelectedValue(event.target.value);
   };
   
   return (
     <div>
-      <h3>Выберите вашу возрастную группу:</h3>
-      <select value={ageGroup} onChange={handleChange}>
-        <option value="">-- Выберите возраст --</option>
-        <option value="0-12">от 0 до 12 лет</option>
-        <option value="13-17">от 13 до 17 лет</option>
-        <option value="18-25">от 18 до 25 лет</option>
-        <option value="25+">старше 25 лет</option>
-      </select>
+      <label>
+        <input
+          type="radio"
+          name="radioGroup"
+          value="1"
+          checked={selectedValue === '1'}
+          onChange={handleChange}
+        />
+        Вариант 1
+      </label>
       
-      {ageGroup && (
-        <p>
-          {ageGroup === '0-12' && 'Вы выбрали группу: дети (0-12 лет)'}
-          {ageGroup === '13-17' && 'Вы выбрали группу: подростки (13-17 лет)'}
-          {ageGroup === '18-25' && 'Вы выбрали группу: молодежь (18-25 лет)'}
-          {ageGroup === '25+' && 'Вы выбрали группу: взрослые (25+ лет)'}
-        </p>
-      )}
+      <label>
+        <input
+          type="radio"
+          name="radioGroup"
+          value="2"
+          checked={selectedValue === '2'}
+          onChange={handleChange}
+        />
+        Вариант 2
+      </label>
+      
+      <label>
+        <input
+          type="radio"
+          name="radioGroup"
+          value="3"
+          checked={selectedValue === '3'}
+          onChange={handleChange}
+        />
+        Вариант 3
+      </label>
+      
+      <p>Вы выбрали: {selectedValue}</p>
     </div>
   );
 }
-
 
 
 export default App;
