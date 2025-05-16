7d7309339cd2887d4ce393221c58d1f3eccd33c1
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-05-16 12:59:01 +0300

    66

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 
-
 function App() {
-  const cities = ['Москва', 'Санкт-Петербург', 'Новосибирск', 'Екатеринбург', 'Казань'];
-  const [selectedCity, setSelectedCity] = useState('');
+  const [ageGroup, setAgeGroup] = useState('');
   
-  const cityOptions = cities.map((city, index) => (
-    <option key={index} value={city}>
-      {city}
-    </option>
-  ));
-
+  const handleChange = (event) => {
+    setAgeGroup(event.target.value);
+  };
+  
   return (
     <div>
-      <select 
-        value={selectedCity} 
-        onChange={(event) => setSelectedCity(event.target.value)}
-      >
-        <option value="">-- Выберите город --</option>
-        {cityOptions}
+      <h3>Выберите вашу возрастную группу:</h3>
+      <select value={ageGroup} onChange={handleChange}>
+        <option value="">-- Выберите возраст --</option>
+        <option value="0-12">от 0 до 12 лет</option>
+        <option value="13-17">от 13 до 17 лет</option>
+        <option value="18-25">от 18 до 25 лет</option>
+        <option value="25+">старше 25 лет</option>
       </select>
       
-      {selectedCity && (
-        <p>Ваш выбор: {selectedCity}</p>
+      {ageGroup && (
+        <p>
+          {ageGroup === '0-12' && 'Вы выбрали группу: дети (0-12 лет)'}
+          {ageGroup === '13-17' && 'Вы выбрали группу: подростки (13-17 лет)'}
+          {ageGroup === '18-25' && 'Вы выбрали группу: молодежь (18-25 лет)'}
+          {ageGroup === '25+' && 'Вы выбрали группу: взрослые (25+ лет)'}
+        </p>
       )}
     </div>
   );
